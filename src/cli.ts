a0c5cd9c6c5eb1fc290b240367084d44df0f188e
Your Name <you@example.com>
2026-05-14 09:51:26 -0300

    aula prog

---
+++ src/cli.ts
@@ -0,0 +1,2 @@
+const params = process.argv
+const command = params[2]
--- src/core.ts
+++ src/core.ts
@@ -1,5 +1,5 @@
 class Item {
-  constructor(public item: string) { }
+  constructor(public title: string) { }
 }
 
 class TodoList {
@@ -8,18 +8,40 @@ class TodoList {
 
   constructor(filePath: string) {
     this.filePath = filePath;
+    this.loadListFromDisk()
   }
 
-  addItem(item: Item) {
+  private async saveListToDisk() {
+    const file = Bun.file(this.filePath);
+    const data = JSON.stringify(this.itens);
+    await file.write(data);
+  }
+
+  private async loadListFromDisk(){
+    const file = Bun.file(this.filePath)
+    const data = await file.json()
+    this.itens = data.map((v: any) => new Item(v.title))
+  }  
+  
+  /**
+   * função que adiciona um item
+   */
+  async addItem(item: Item) {
     this.itens.push(item)
+    await this.saveListToDisk()
   }
-
-  removeItem(index: number) {
+  /**
+   * função que remove um item
+   */
+  async removeItem(index: number) {
     this.itens.splice(index, 1);
+    await this.saveListToDisk()
   }
-
+  /**
+   * função que retorna a copia dos itens
+   */
   getItems() {
-    return this.itens
+    return [...this.itens]
   }
 }
 
